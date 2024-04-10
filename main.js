80613ff7caa522a5b4b7183742fc182d30d002f3
Matheus Fellipe Turesso <103067397+zTuressOz@users.noreply.github.com>
2024-04-10 09:21:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,44 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo5 = new Date("2024-12-30T00:00:00");
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4,tempoObjetivo5];
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
+    }
+}
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias0").textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas0").textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min0").textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg0").textContent = calculaTempo(tempos[i])[3];
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
